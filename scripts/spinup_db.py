ba023e671939ab6f345956e2839d8ca0034a4fea
Gage Christensen <49492919+grchristensen@users.noreply.github.com>
2024-10-26 23:22:38 -0700

    Implement schema management
    Schemas will now automatically be setup on DB instantiation based on the
    config provided in lifedb/schemas.yml

--- scripts/spinup_db.py
+++ scripts/spinup_db.py
@@ -2,7 +2,9 @@ import os
 from pathlib import Path
 
 import duckdb
+import yaml
 from dotenv import find_dotenv, load_dotenv
+from importlib_resources import files
 
 from lifedb import DBError
 
@@ -21,7 +23,13 @@ def main():
 
     # Connecting to a DuckDB database will automatically create one if it does not
     # exist.
-    duckdb.connect(db_path)
+    db = duckdb.connect(db_path)
+
+    schema_yaml = files("lifedb").joinpath("schemas.yml").read_text()
+    schema_yaml = yaml.safe_load(schema_yaml)
+
+    for schema in schema_yaml["schemas"]:
+        db.execute(f"create schema {schema}")
 
 
 if __name__ == "__main__":
